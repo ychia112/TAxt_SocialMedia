8bb02ad7d22eaa004f4edaa97ec2e7f1e4803452
鄭又嘉 <ychia@YchiaMac.local>
2023-01-29 16:31:39 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/Ychia/test/ios_proj01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.Ychia.test.ios_proj01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new Center(
+    child: new Text(
+      'First app', textDirection: TextDirection.ltr,
+    )
+  ));
+}
+
