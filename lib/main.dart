04b4c0a6989d376daa74bf45dd418b63ad63a9c1
鄭又嘉 <ychia@YchiaMac.mshome.net>
2023-02-01 16:31:43 +0800

    initial main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:social_media/homepage.dart';
 
 void main() {
-  runApp(new Center(
-    child: new Text(
-      'First app', textDirection: TextDirection.ltr,
-    )
-  ));
-}
+  runApp(MyApp());
+  }
 
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          primaryColor: Colors.black,
+          accentColor: Colors.white,
+          dividerColor: Colors.black12,
+          brightness: Brightness.dark),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
