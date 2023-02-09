39b91bf650e456ee59c369ff377e14706e56cb8d
chengcheng <cerulean.ycc@gmail.com>
2023-02-09 20:41:21 +0800

    update interface of home and post

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,43 +7,173 @@ class UserHome extends StatelessWidget {
   Widget build(BuildContext context) {
     if (storing.isNotEmpty){
       return Scaffold(
-          body: CustomScrollView(
-              slivers: [
-                SliverAppBar(
-                  pinned: true,
-                  flexibleSpace: FlexibleSpaceBar(
-                    background: Container(
-                      color: Colors.black,
-                    ),
-                    title: Text('A p p N a m e '),
-                  ),
-                ),
-                SliverSafeArea(sliver:
-                  SliverGrid(
-                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 1,
-                        crossAxisSpacing: 10,
-                        mainAxisSpacing: 10,
-                        childAspectRatio: 2
-                    ),
-                    delegate: SliverChildBuilderDelegate(
-                          (BuildContext context, int index) {
-                            const Padding(padding: EdgeInsets.symmetric(horizontal: 32, vertical: 6));
-                        return ClipRRect(
-                          borderRadius: BorderRadius.circular(24),
-                          child: Container(
-                            color: Colors.grey.shade200,
-                            alignment: Alignment.center,
-                            child:Text("${storing[index]}"),
-                          )
-                        );
-                      },
-                      childCount:storing.length,
+          resizeToAvoidBottomInset: false,
+          appBar: AppBar(
+            leading: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.add_box_outlined,size: 30,),
+              alignment: Alignment.center,
+            ),
+            flexibleSpace: FlexibleSpaceBar(
+              background: Container(
+                color: Colors.black,
+              ),
+              title: const Text('A p p N a m e '),
+              centerTitle: true
+            ),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.account_circle_outlined,size: 30,),
+                alignment: Alignment.centerLeft,
+              ),
+            ],
+          ),
+          body:
+            Padding(
+            padding: const EdgeInsets.only(right :12, left: 12),
+            child:
+              SingleChildScrollView(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height-150,
+                    width: MediaQuery.of(context).size.width,
+                    color: Colors.grey.shade50,
+                    child:
+                    ListView.separated(
+                      itemCount: storing.length,
+                      padding: const EdgeInsets.only(top:15.0,bottom:15.0),
+                      separatorBuilder: (BuildContext context,int index)=>
+                      const Divider(height: 16,color: Color(0xFFFFFFFF)),
+                      itemBuilder: (BuildContext context, int index) {
+                        return Container(
+                          alignment: Alignment.center,
+                          // tileColor: _items[index].isOdd ? oddItemColor : evenItemColor,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(24.0),
+                            color:  Colors.grey.shade300,
+                          ),
+                        child:
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Row(
 
-                    ),
-                  ),
+                              children:[
+                                const Padding(padding: EdgeInsets.only(top:60.0,left: 10)),
+                                ClipOval(
+                                    child:
+                                      Image.asset('assets/images/2.jpg',width: 50,height: 50,fit: BoxFit.cover,)
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Center(
+                                    child: Container(
+                                      alignment: Alignment.center,
+                                      constraints: const BoxConstraints(
+                                          maxWidth: 515, maxHeight: 250, minWidth: 515, minHeight: 200
+                                      ),//should be more precise
+                                      decoration: BoxDecoration(
+                                        //borderRadius: BorderRadius.circular(24.0),
+                                        color:  Colors.grey.shade200,),
+                                      child:
+                                        Text(
+                                            storing[index], textAlign: TextAlign.center,
+                                            maxLines: 10),
+                                    )
+                                )
+
+                                ],
+                            ),
+                            Row(
+                              children: [
+                               // const Padding(padding: EdgeInsets.only(top:20.0,bottom: 20)),
+                                IconButton(
+                                  icon: const Icon(Icons.add_circle,size: 30,color: Colors.black54,),
+                                  onPressed: (){},
+                                ),
+                                const Padding(padding: EdgeInsets.only(left: 420)),
+                                IconButton(
+                                  icon: const Icon(Icons.account_circle,size: 30,color: Colors.black54,),
+                                  onPressed: (){},
+                                  alignment: Alignment.bottomRight,
+                                ),
+                              ],
+                            ),
+                    ],
+                    )
+
+    );
+    }
+
+    ),
 
                 ),
+             // slivers: [
+                // SliverAppBar(
+                //   pinned: true,
+                //   flexibleSpace: FlexibleSpaceBar(
+                //     background: Container(
+                //       color: Colors.black,
+                //     ),
+                //     title: const Text('A p p N a m e '),
+                //   ),
+                // ),
+              //   SliverSafeArea(sliver:
+              //     SliverGrid(
+              //       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              //           crossAxisCount: 1,
+              //           crossAxisSpacing: 15,
+              //           mainAxisSpacing:15,
+              //           childAspectRatio: 3
+              //       ),
+              //       delegate: SliverChildBuilderDelegate(
+              //             (BuildContext context, int index) {
+              //               const Padding(padding: EdgeInsets.symmetric(vertical: 10,horizontal: 10));
+              //           return Container(
+              //             //padding: const EdgeInsets.only(top:80.0,bottom: 80),//size of block
+              //             alignment: Alignment.center,
+              //             // tileColor: _items[index].isOdd ? oddItemColor : evenItemColor,
+              //             decoration: BoxDecoration(
+              //               borderRadius: BorderRadius.circular(24.0),
+              //               color:  Colors.grey.shade200,
+              //             ),
+              //             child:
+              //             Column(
+              //               children: [
+              //                 Row(
+              //                   children: [
+              //                     Padding(
+              //                         padding: const EdgeInsets.only(top:80.0,bottom: 80),
+              //                       child:Text("${storing[index]}",),
+              //                     ),
+              //                   ],
+              //                 ),
+              //                 Row(
+              //                   children: [
+              //                     IconButton(
+              //                       icon: const Icon(Icons.add_circle,size: 5,),
+              //                       onPressed: (){},
+              //                     ),
+              //                   ],
+              //                 ),
+              //
+              //               ],
+              //             ),
+              //
+              //           );
+              //         },
+              //         childCount:storing.length,
+              //       ),
+              //     ),
+              //
+
               ]
                 // SliverToBoxAdapter(
                 //     child: Padding(
@@ -62,7 +192,8 @@ class UserHome extends StatelessWidget {
 
 
           )
-
+    ),
+            )
     );
     }
     else {
--- lib/pages/post.dart
+++ lib/pages/post.dart
@@ -13,7 +13,7 @@ class _UserPostState extends State<UserPost> {
   final _textController = TextEditingController();
   // store the input text
   String userPost = '';
-  //final List _userpost = <String>[];
+
 
 
   @override
@@ -23,83 +23,152 @@ class _UserPostState extends State<UserPost> {
         FocusScope.of(context).unfocus(),
       },
       child: Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+
+        flexibleSpace: FlexibleSpaceBar(
+          background: Container(
+            color: Colors.black,
+          ),
+          title: const Text('Y o u r t e x t'),
+          centerTitle: true
+        ),
+      ),
       body: Padding(
-        padding: const EdgeInsets.only(left: 24, right: 24, bottom: 90),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            // display text
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 6),
-                  child: Container(
-                    height: 450,
-                    color: Colors.grey.shade200,
-                    child:
-                        ListView.separated(
-                        itemCount: _userpost.length,
-                        padding: const EdgeInsets.only(top:15.0),
-                        separatorBuilder: (BuildContext context,int index)=>
-                          const Divider(height: 16,color: Color(0xFFFFFFFF)),
+        padding: const EdgeInsets.only(bottom: 30,right :12, left: 12),
+        child:
+          SingleChildScrollView(
+            child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height-215,
+                width: MediaQuery.of(context).size.width,
+                color: Colors.grey.shade50,
+                child:
+                ListView.separated(
+                    itemCount: _userpost.length,
+                    padding: const EdgeInsets.only(top:15.0,bottom:15 ),
+                    separatorBuilder: (BuildContext context,int index)=>
+                    const Divider(height: 16,color: Color(0xFFFFFFFF)),
+                    itemBuilder: (BuildContext context, int index) {
+                      if (_userpost.isEmpty){
+                      }
+                      else {
+                        return Container(
+                          //padding: const EdgeInsets.only(top:80.0,bottom: 80),//size of block
+                          alignment: Alignment.center,
+                          // tileColor: _items[index].isOdd ? oddItemColor : evenItemColor,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(24.0),
+                            color:  Colors.grey.shade200,
+                          ),
+                          child:
+                              Column(
+                                children: [
+                                  Row(
+                                    children: [
 
-                        itemBuilder: (BuildContext context, int index) {
-                          if (_userpost.isEmpty){
-                          }
-                          else {
-                            return Container(
-                              padding: const EdgeInsets.all(16.0),
-                              alignment: Alignment.center,
-                              // tileColor: _items[index].isOdd ? oddItemColor : evenItemColor,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(15.0),
-                                color: Colors.black12,
-                              ),
-                              child: Text(
-                                  _userpost[index], textAlign: TextAlign.left,
-                                  maxLines: 10),
-                            );
+                                    Center(
+                                      child: Container(
+                                        alignment: Alignment.center,
+                                        constraints: const BoxConstraints(
+                                            maxWidth: 515, maxHeight: 250, minWidth: 515, minHeight: 200
+                                        ),//should be more precise
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(24.0),
+                                          color:  Colors.grey.shade300,),
+                                        child:
+                                        Text(
+                                            _userpost[index], textAlign: TextAlign.center,
+                                            maxLines: 10),
+                                    )
+                                  ),
 
-                            }
-                          }
+                                  ]
+                                  ),
+                                  Row(
+                                    children: [
+                                      IconButton(
+                                        icon: const Icon(Icons.add_circle,size: 30,),
+                                        onPressed: (){},
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              )
 
-                      ),
-                    ),
-            ),
-            // input text
-            TextFormField(
-                minLines: 1,
-                maxLines: 3,
-                keyboardType: TextInputType.multiline,
-                controller: _textController,
-                decoration: InputDecoration(
-                  hintText: 'How is your day?',
-                  border: const OutlineInputBorder(),
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      _textController.clear();
 
-                    },
-                    icon: const Icon(Icons.clear),
-                  ),
+                        );
+                      }
+                    }
                 ),
               ),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
 
-            // send text
-            IconButton(
-              icon: const Icon(Icons.send_rounded),
-              onPressed: (){
-                setState(() {
-                  userPost=_textController.text;
-                  _userpost.add(_textController.text) ;
-                  timecount++;
-                });
-              },
-              color: Colors.black45,
-            )
-          ],
+                  children:[
+                    SizedBox(
+                      width: 460,
+                      height: 80,
+                      child:
+                      TextFormField(
+                        minLines: 1,
+                        maxLines: 3,
+                        keyboardType: TextInputType.multiline,
+                        controller: _textController,
+                        decoration: InputDecoration(
+                          hintText: 'How is your day?',
+                          //contentPadding: const EdgeInsets.all(18.0),
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                              borderSide:
+                                const BorderSide( width: 3,color: Colors.black12),
+                          ),
+                          prefixIconConstraints: const BoxConstraints(
+                              minWidth: 8
+                          ),
+                          suffixIcon: IconButton(
+                            onPressed: () {
+                              _textController.clear();
+                            },
+                            icon: const Icon(Icons.clear),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 40,
+                      height: 60,
+                      child: IconButton(
+                        icon: const Icon(Icons.send_rounded),
+                        onPressed: (){
+                          setState(() {
+                            userPost=_textController.text;
+                            _textController.clear();
+                            _userpost.add(userPost) ;
+                            timecount++;
+                          });
+                        },
+                        color: Colors.black45,
+                        alignment: Alignment.centerRight,
+                      ),
+                    ),
+                  ]
+              )
+              // display text
+              // input text
+              // send text
+            ],
+          ),
         ),
+
+
       )
     ),
+
     );
   }
 }
