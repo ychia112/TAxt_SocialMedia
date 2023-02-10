7df166574aeb44d7fa5d275a7607621e71dfba42
ychia <jackyjacky63@gmail.com>
2023-02-10 22:01:59 +0800

    Merge remote-tracking branch 'origin/new_interface_for_home_and_post'
    # Conflicts:
    #	lib/homepage.dart
    #	lib/main.dart
    #	lib/pages/home.dart
    #	lib/pages/post.dart
    #	lib/pages/profile.dart
    #	pubspec.lock
    #	pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,7 @@ void main(){
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-    return MaterialApp(
-=======
     return const MaterialApp(
->>>>>>> origin/new_interface_for_home_and_post
       home: HomePage(),
     );
   }
