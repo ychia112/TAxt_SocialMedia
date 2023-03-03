c76b3b1fa549fbbc0c6c7cfe3b36c608ae86235c
ychia <jackyjacky63@gmail.com>
2023-03-04 00:32:41 +0800

    edit profile page

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -4,11 +4,11 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:ios_proj01/providers/metamask_provider.dart';
-import 'package:ios_proj01/widgets/post_viewing.dart';
 import 'package:provider/provider.dart';
 import 'home.dart';
 import '../utils/mood.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:ios_proj01/widgets/post_viewing.dart';
 
 class UserProfile extends StatefulWidget {
   const UserProfile({Key? key}) : super(key: key);
@@ -29,28 +29,32 @@ class _UserProfileState extends State<UserProfile> {
   
   @override
   Widget build(BuildContext context) {
+    final top = coverHeight - profileHeight / 2;
+    late final Future<List<dynamic>> _posts = getUserPosts(context);
+
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: false,
-          automaticallyImplyLeading: false,
-          elevation: 0,
-          backgroundColor: Colors.transparent,
-          // leading:
-          title: Text(
-            'Profile',
-            style: GoogleFonts.abrilFatface(
-                textStyle: TextStyle(
-                    fontSize: 48,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black
-                )),
-          ),
+      appBar: AppBar(
+        centerTitle: false,
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        // leading:
+        title: Text(
+          'Profile',
+          style: GoogleFonts.abrilFatface(
+              textStyle: TextStyle(
+                  fontSize: 48,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black
+              )),
         ),
+      ),
       body: ListView(
+        physics: NeverScrollableScrollPhysics(),
         padding: EdgeInsets.zero,
         children: <Widget>[
           Container(height: 12,),
-          const SizedBox(height: 5,),
+          buildPosts(_posts),
           const PostViewingWidget(),
         ],
       ),
@@ -61,46 +65,49 @@ class _UserProfileState extends State<UserProfile> {
     children: [
       const SizedBox(height: 8),
       Text(
-        '@'+context.read<MetaMask>().session.accounts[0],
+        context.read<MetaMask>().session.accounts[0],
         style: const TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
       ),
       const SizedBox(height: 10,),
+      const Text(
+        '@UserLocation',
+        style: TextStyle(fontSize: 16, color: Colors.grey),
+      )
     ],
   );
 
   Widget buildProfileImage() => CircleAvatar(
     radius: profileHeight / 2,
     backgroundColor: Colors.grey.shade800,
-    backgroundImage: const AssetImage('assets/images/2.jpg')
+    backgroundImage: AssetImage('assets/images/2.jpg')
   );
 
   Widget buildPosts(_posts) => FutureBuilder<List<dynamic>>(
     future: _posts,
     builder: (context, snapshot) {
       if(snapshot.hasData){
-        return Padding(
+        return SingleChildScrollView(
           padding: const EdgeInsets.all(12),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              const MyStatefulWidget(),
               Container(
                 height: MediaQuery.of(context).size.height-260,
                 width: MediaQuery.of(context).size.width,
-                color: Colors.transparent,
-                child: ListView.separated(
+                color: Colors.grey.shade50,
+                child:
+                ListView.separated(
                   itemCount: snapshot.data!.length,
                   padding: const EdgeInsets.only(top:5.0,bottom:15.0),
                   separatorBuilder: (BuildContext context,int index)=>
-                  const Divider(height: 16,color: Colors.transparent),
+                  const Divider(height: 16,color: Color(0xFFFFFFFF)),
                   itemBuilder: (BuildContext context, int index) {
                     if (index == 0){
                       return Column(
                         children: [
                           buildProfileImage(),
                           buildContent(context),
-
                         ],
                       );
                     }
